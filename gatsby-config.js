70242cd9eb8ba2866dfb758b631ec837c4c9cf18
GabrielFagundes <aluzgabriel@hotmail.com>
2019-12-08 18:49:00 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Intronauta - Travel to the inner`,
+    description: `A blog about inner knowledge`,
+    author: `@gabrielfagundes`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
