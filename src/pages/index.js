655fe60c59cedc3d6d2188a442632906f76fea4e
Gabriel Fagundes <gabriel.fagundes@compasso.com.br>
2020-01-21 11:03:50 -0300

    Create base layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
   </Layout>
 )
 
